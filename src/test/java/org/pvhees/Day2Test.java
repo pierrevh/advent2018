495e7fcd3e296ca9323c68928b285f86574ec1e2
Pierre van Hees <pvhees@xs4all.nl>
2018-12-09 14:50:35 +0100

    refactored day2, part1 solution to day2, part2

--- src/test/java/org/pvhees/Day2Test.java
+++ src/test/java/org/pvhees/Day2Test.java
@@ -7,27 +7,25 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertEquals;
+
 public class Day2Test {
 
     @Test
-    public void givenInAssignment() {
-        List<String> testcases = Arrays.asList("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab");
-        System.out.println("Checksum on list = " + checksum(testcases));
+    public void part1Example() {
+        List<String> boxIds = Arrays.asList("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab");
+        assertEquals(12, checksum(boxIds));
     }
 
     @Test
-    public void partOne() {
-        List<String> testcases = readFileOfStrings("inputday2.txt");
-        System.out.println("Checksum on list = " + checksum(testcases));
-
+    public void part1() {
+        List<String> boxIds = readFileOfStrings("inputday2.txt");
+        System.out.println("Part 1, checksum = " + checksum(boxIds));
     }
 
     private long checksum(List<String> input) {
@@ -43,7 +41,7 @@ public class Day2Test {
 
     private boolean[] checksumString(String s) {
         Map<String, Long> checksumParts = checksumEntries(s);
-        return new boolean [] {
+        return new boolean[]{
                 checksumParts.values().stream().filter(v -> v == 2).count() > 0,
                 checksumParts.values().stream().filter(v -> v == 3).count() > 0
         };
@@ -67,5 +65,65 @@ public class Day2Test {
             e.printStackTrace();
         }
         return Collections.emptyList();
+    }
+
+    @Test
+    public void part2Example() {
+        String expected = "fgij";
+        List<String> boxIds = Arrays.asList("abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz");
+        String commonLetters = findCommonLetters(boxIds);
+        assertEquals(expected, commonLetters);
+    }
+
+    @Test
+    public void part2() {
+        List<String> boxIds = readFileOfStrings("inputday2.txt");
+        String commonLetters = findCommonLetters(boxIds);
+        System.out.println("Part2, common letters = " + commonLetters);
+    }
+
+    private String findCommonLetters(List<String> boxIds) {
+        List<String> commonBoxIds = findCommonBoxIds(boxIds);
+        return matchingLetters(commonBoxIds);
+    }
+
+    private List<String> findCommonBoxIds(List<String> boxIds) {
+        for (int i = 0; i < boxIds.size(); i++) {
+            for (int j = 0; j < boxIds.size(); j++) {
+                if (i != j) {
+                    if (matches(boxIds.get(i), boxIds.get(j))) {
+                        return Arrays.asList(boxIds.get(i), boxIds.get(j));
+                    }
+                }
+            }
+
+        }
+        return null;
+    }
+
+    private boolean matches(String s1, String s2) {
+        if (s1.length() != s2.length())
+            throw new IllegalArgumentException(String.format("Strings moeten gelijke lengte hebben (%s, %s)", s1, s2));
+        int matchingLetterCount = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
+                matchingLetterCount++;
+            }
+        }
+
+        return matchingLetterCount == (s1.length() - 1);
+    }
+
+    private String matchingLetters(List<String> commonBoxIds) {
+        String id1 = commonBoxIds.get(0);
+        String id2 = commonBoxIds.get(1);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < id1.length(); i++) {
+            if (id1.charAt(i) == id2.charAt(i)) {
+                result.append(id1.charAt(i));
+            }
+        }
+
+        return result.toString();
     }
 }
